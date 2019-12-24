1a34fd7f1ea132e7652a88ee64d00f1aae66310a
kajan <kajan@thesoftwarepractice.com>
2019-12-24 16:25:12 +0530

    01-defining primitive data types

--- main.go
+++ main.go
@@ -6,4 +6,14 @@ import (
 
 func main() {
 	fmt.Println("Hello World")
+
+	var i int
+	i = 4
+	fmt.Println(i)
+
+	var p float32 = 2.2
+	fmt.Println(p)
+
+	firstName := "Mike"
+	fmt.Println(firstName)
 }
