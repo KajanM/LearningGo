2d03c5d7b8ee033ac6054863160b7e625a622abe
kajan <kajan@thesoftwarepractice.com>
2019-12-25 16:24:26 +0530

    04-add more functions to user webservice

--- models/user.go
+++ models/user.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"errors"
+	"fmt"
+)
+
 type User struct {
 	Id        int
 	FirstName string
@@ -16,8 +21,45 @@ func GetUsers() []*User {
 }
 
 func AddUser(user User) (User, error) {
+	if user.Id == 0 {
+		return User{}, errors.New("new User must not include id")
+	}
 	user.Id = nextId
 	nextId++
 	users = append(users, &user)
 	return user, nil
+}
+
+func GetUserById(id int) (User, error) {
+	for _, u := range users {
+		if u.Id == id {
+			return *u, nil
+		}
+	}
+
+	return User{}, fmt.Errorf("user with Id '%v' not found", id)
+}
+
+func UpdateUser(u User) (User, error) {
+	if u.Id == 0 {
+		return User{}, errors.New("id is required")
+	}
+	for i, candidate := range users {
+		if candidate.Id == id {
+			users[i] = &u
+			return u, nil
+		}
+	}
+	return User{}, fmt.Errorf("could not find a user with id '%v'", id)
+}
+
+func RemoveUserById(id int) error {
+	for i, u := range users {
+		if u.Id == id {
+			users = append(users[:i], users[i+1:]...)
+			return nil
+		}
+	}
+
+	return fmt.Errorf("user with id '%v' not found", id)
 }
